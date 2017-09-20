8b7ba9d7223ace0b00f7af7894b633be5651c379
Kevin Ball <kmball11@gmail.com>
2017-09-19 20:58:39 -0700

    Get demo page working

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,55 @@
 var gulp    = require('gulp');
 var plugins = require('gulp-load-plugins');
+var browser = require('browser-sync');
 
 var $ = plugins();
 
-var ENTRIES = ['scss/app.scss'];
+var ENTRIES = ['demo/demo.scss'];
 
 var COMPATIBILITY = [ "last 2 version", "ie >= 9", "ios >= 7" ];
 
 // Build the SCSS into CSS
-gulp.task('build', sass);
+gulp.task('build', gulp.parallel(copy, sass));
 
 // Build the SCSS, watch for changes.
 gulp.task('default',
-  gulp.series('build', watch));
+gulp.series('build', server, watch));
+
+// Copy demo index file
+function copy() {
+  return gulp.src(['demo/index.html'])
+    .pipe(gulp.dest('dist'));
+}
 
 function sass() {
   return gulp.src(ENTRIES)
     .pipe($.sourcemaps.init())
-    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.sass({
+      includePaths: ['scss']
+    }).on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: COMPATIBILITY
     }))
     .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browser.reload({ stream: true }));
+  }
+
+// Start a server with BrowserSync to preview the site in
+function server(done) {
+  browser.init({
+    server: 'dist', port: 8000
+  });
+  done();
+}
+
+// Reload the browser with BrowserSync
+function reload(done) {
+  browser.reload();
+  done();
 }
 // Watch for changes to static assets, pages, Sass, and JavaScript
 function watch() {
-  gulp.watch('scss/**/*.scss').on('all', sass);
+  gulp.watch(['scss/**/*.scss', 'demo/*.scss']).on('all', sass);
+  gulp.watch('demo/*.html').on('all', gulp.series(copy, browser.reload));
 }
